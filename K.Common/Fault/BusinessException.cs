7c035f012c1096ff7e83c4b3f82e08fcc0356ebc
kurnia wirawan <kurnia.wirawan@gmail.com>
2013-03-19 18:29:32 +0700

    Add Custom Class Exception

---
+++ K.Common/Fault/BusinessException.cs
@@ -0,0 +1,57 @@
+﻿// CreatedBy	:  kun
+// Solution 	:  KYourMovement 
+// Projectt		:  K.Common
+// FileName		:  BusinessException.cs
+// Created Date	:  2013 03 19 6:28 PM
+
+using System;
+using System.Text;
+using K.Common.Interfaces;
+
+namespace K.Common.Fault
+{
+	[Serializable]
+	public class BusinessException<T> : Exception where T : class, IBaseModel
+	{
+		public BusinessException(string message)
+			: base(message)
+		{
+		}
+
+		public BusinessException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+
+		public BusinessException(string message, T model)
+			: base(message)
+		{
+			BusinessModel = model;
+		}
+
+		public BusinessException(string message, T model, Exception innerException)
+			: base(message, innerException)
+		{
+			BusinessModel = model;
+		}
+
+		public override string StackTrace
+		{
+			get
+			{
+				var result = new StringBuilder();
+				if (BusinessModel != null)
+				{
+					foreach (var b in BusinessModel.GetType().GetProperties())
+					{
+						result.AppendLine("\t" + b.Name + ": " + b.GetValue(BusinessModel, null));
+					}
+				}
+				var a = result + base.StackTrace;
+				return a;
+			}
+		}
+
+		public T BusinessModel { get; set; }
+	}
+}
---
+++ K.Common/Fault/NoAccessMethodException.cs
@@ -0,0 +1,43 @@
+﻿// CreatedBy	:  kun
+// Solution 	:  KYourMovement 
+// Projectt		:  K.Common
+// FileName		:  NoAccessMethodException.cs
+// Created Date	:  2013 03 19 6:28 PM
+
+using System;
+using System.Reflection;
+using System.Text;
+
+namespace K.Common.Fault
+{
+	public class NoAccessMethodException : Exception
+	{
+		public NoAccessMethodException(string message)
+			: base(message)
+		{
+		}
+
+		public NoAccessMethodException(string message, MethodBase methodBase)
+			: base(message)
+		{
+			CurrentMethodBase = methodBase;
+		}
+
+		public override string StackTrace
+		{
+			get
+			{
+				var result = new StringBuilder();
+				if (CurrentMethodBase != null)
+				{
+					result.AppendLine("\tMethodName: " + CurrentMethodBase.Name);
+					result.AppendLine("\tModule: " + CurrentMethodBase.Module);
+				}
+				var a = result + base.StackTrace;
+				return a;
+			}
+		}
+
+		internal MethodBase CurrentMethodBase { get; set; }
+	}
+}
